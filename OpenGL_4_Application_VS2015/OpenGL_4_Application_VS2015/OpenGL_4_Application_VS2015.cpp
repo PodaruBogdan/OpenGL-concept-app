e582381ea5c4396a2a8919611c694fc27806d4a9
PodaruBogdan <48868233+PodaruBogdan@users.noreply.github.com>
2020-02-18 00:38:02 +0200

    add all files

---
+++ OpenGL_4_Application_VS2015/OpenGL_4_Application_VS2015/OpenGL_4_Application_VS2015.cpp
@@ -0,0 +1,621 @@
+//
+//  main.cpp
+//  OpenGL Shadows
+//
+//  Created by CGIS on 05/12/16.
+//  Copyright � 2016 CGIS. All rights reserved.
+//
+
+#define GLEW_STATIC
+
+#include <iostream>
+#include <time.h>
+#include "glm/glm.hpp"//core glm functionality
+#include "glm/gtc/matrix_transform.hpp"//glm extension for generating common transformation matrices
+#include "glm/gtc/matrix_inverse.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "GLEW/glew.h"
+#include "GLFW/glfw3.h"
+#include <string>
+#include "Shader.hpp"
+#include "Camera.hpp"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "SkyBox.hpp"
+
+#include "Model3D.hpp"
+#include "Mesh.hpp"
+
+int glWindowWidth = 640;
+int glWindowHeight = 480;
+int retina_width, retina_height;
+GLFWwindow* glWindow = NULL;
+
+const GLuint SHADOW_WIDTH = 4096, SHADOW_HEIGHT = 4096;
+
+glm::mat4 model;
+GLuint modelLoc;
+glm::mat4 view;
+GLuint viewLoc;
+glm::mat4 projection;
+GLuint projectionLoc;
+glm::mat3 normalMatrix;
+GLuint normalMatrixLoc;
+glm::mat3 lightDirMatrix;
+GLuint lightDirMatrixLoc;
+
+glm::vec3 lightDir;
+GLuint lightDirLoc;
+glm::vec3 lightColor;
+GLuint lightColorLoc;
+
+gps::Camera myCamera(glm::vec3(-10.0f, 20.0f, 45.0f), glm::vec3(0.0f, 5.0f, 0.0f));
+GLfloat cameraSpeed = 0.05f;
+
+bool pressedKeys[1024];
+GLfloat angle;
+GLfloat sateliteAngle;
+GLfloat lightAngle;
+
+gps::Model3D myModel;
+gps::Model3D scene;
+gps::Model3D satelite;
+gps::Model3D lightCube;
+gps::Shader myCustomShader;
+gps::Shader lightShader;
+gps::Shader depthMapShader;
+GLuint shadowMapFBO;
+GLuint depthMapTexture;
+bool canControl = false;
+
+
+float fov = 45.0f;
+GLuint textureID;
+gps::SkyBox mySkyBox;
+gps::Shader skyboxShader;
+gps::Model3D gloves;
+
+GLenum glCheckError_(const char* file, int line)
+{
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR)
+	{
+		std::string error;
+		switch (errorCode)
+		{
+		case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+		case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+		case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+		case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+		case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+		case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+		}
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+	fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
+	//TODO
+	//for RETINA display
+	glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+
+	myCustomShader.useShaderProgram();
+
+	//set projection matrix
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+	//send matrix data to shader
+	GLint projLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	lightShader.useShaderProgram();
+
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	//set Viewport transform
+	glViewport(0, 0, retina_width, retina_height);
+}
+int toggle_fog = 0;
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			pressedKeys[key] = true;
+		else if (action == GLFW_RELEASE)
+			pressedKeys[key] = false;
+	}
+
+	if (glfwGetKey(glWindow, GLFW_KEY_F))
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	if (glfwGetKey(glWindow, GLFW_KEY_G))
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+	if (glfwGetKey(glWindow, GLFW_KEY_H))
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+	}
+	if (glfwGetKey(glWindow, GLFW_KEY_SPACE))
+	{
+		toggle_fog = 1-toggle_fog;
+	}
+}
+// camera
+glm::vec3 cameraPos = glm::vec3(1.0f, 1.0f, 1.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+void mouseCallback(GLFWwindow * window, double xpos, double ypos)
+{
+	if (canControl) {
+		if (firstMouse)
+		{
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+		lastX = xpos;
+		lastY = ypos;
+
+		float sensitivity = 0.1f; // change this value to your liking
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		yaw += xoffset;
+		pitch += yoffset;
+
+		// make sure that when pitch is out of bounds, screen doesn't get flipped
+		if (pitch > 89.0f)
+			pitch = 89.0f;
+		if (pitch < -89.0f)
+			pitch = -89.0f;
+
+		myCamera.rotate(pitch, yaw);
+	}
+}
+
+void processMovement()
+{
+
+	if (pressedKeys[GLFW_KEY_Q]) {
+		angle += 1.0f;
+		if (angle > 360.0f)
+			angle -= 360.0f;
+	}
+
+	if (pressedKeys[GLFW_KEY_E]) {
+		angle -= 1.0f;
+		if (angle < 0.0f)
+			angle += 360.0f;
+	}
+
+	if (pressedKeys[GLFW_KEY_W]) {
+		myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_S]) {
+		myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_A]) {
+		myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_D]) {
+		myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_J]) {
+
+		lightAngle += 0.3f;
+		if (lightAngle > 360.0f)
+			lightAngle -= 360.0f;
+		glm::vec3 lightDirTr = glm::vec3(glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(lightDir, 1.0f));
+		myCustomShader.useShaderProgram();
+		glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDirTr));
+	}
+
+	if (pressedKeys[GLFW_KEY_L]) {
+		lightAngle -= 0.3f;
+		if (lightAngle < 0.0f)
+			lightAngle += 360.0f;
+		glm::vec3 lightDirTr = glm::vec3(glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(lightDir, 1.0f));
+		myCustomShader.useShaderProgram();
+		glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDirTr));
+	}
+
+}
+
+
+
+bool initOpenGLWindow()
+{
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return false;
+	}
+	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	//for Mac OS X
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	glWindow = glfwCreateWindow(mode->width, mode->height, "My Title", glfwGetPrimaryMonitor(), NULL);
+	if (!glWindow) {
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return false;
+	}
+
+	glfwSetWindowSizeCallback(glWindow, windowResizeCallback);
+	glfwMakeContextCurrent(glWindow);
+
+	// start GLEW extension handler
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// get version info
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	//for RETINA display
+	glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+
+	glfwSetKeyCallback(glWindow, keyboardCallback);
+	glfwSetCursorPosCallback(glWindow, mouseCallback);
+
+	return true;
+}
+
+void initOpenGLState()
+{
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glViewport(0, 0, retina_width, retina_height);
+	glEnable(GL_FRAMEBUFFER_SRGB);
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glEnable(GL_CULL_FACE); // cull face
+	glCullFace(GL_BACK); // cull back face
+	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+}
+
+void initFBOs()
+{
+	//generate FBO ID
+	glGenFramebuffers(1, &shadowMapFBO);
+
+	//create depth texture for FBO
+	glGenTextures(1, &depthMapTexture);
+	glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+		SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	//attach texture to FBO
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMapTexture, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	//skybox texture
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+}
+
+glm::mat4 computeLightSpaceTrMatrix()
+{
+	const GLfloat near_plane = 1.0f, far_plane = 20.0f;
+	glm::mat4 lightProjection = glm::ortho(-20.0f, 20.0f, -20.0f, 20.0f, near_plane, far_plane);
+
+	glm::vec3 lightDirTr = glm::vec3(glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(lightDir, 1.0f));
+	glm::mat4 lightView = glm::lookAt(myCamera.getCameraTarget() + 1.0f * lightDirTr, myCamera.getCameraTarget(), glm::vec3(0.0f, 1.0f, 1.0f));
+
+	return lightProjection * lightView;
+}
+
+void initModels()
+{
+	myModel = gps::Model3D("objects/hope/Scene.obj", "objects/hope/");
+	lightCube = gps::Model3D("objects/cube/cube.obj", "objects/cube/");
+	satelite = gps::Model3D("objects/Sat/Satellite.obj", "objects/Sat/");
+
+}
+
+void initShaders()
+{
+	myCustomShader.loadShader("shaders/shaderStart.vert", "shaders/shaderStart.frag");
+	lightShader.loadShader("shaders/lightCube.vert", "shaders/lightCube.frag");
+	depthMapShader.loadShader("shaders/simpleDepthMap.vert", "shaders/simpleDepthMap.frag");
+}
+
+void initUniforms()
+{
+	myCustomShader.useShaderProgram();
+
+	modelLoc = glGetUniformLocation(myCustomShader.shaderProgram, "model");
+
+	viewLoc = glGetUniformLocation(myCustomShader.shaderProgram, "view");
+
+	normalMatrixLoc = glGetUniformLocation(myCustomShader.shaderProgram, "normalMatrix");
+
+	lightDirMatrixLoc = glGetUniformLocation(myCustomShader.shaderProgram, "lightDirMatrix");
+
+	projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+	projectionLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	//set the light direction (direction towards the light)
+	lightDir = glm::vec3(0.0f, 1.0f, 2.0f);
+	lightDirLoc = glGetUniformLocation(myCustomShader.shaderProgram, "lightDir");
+	glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+
+	//set light color
+	lightColor = glm::vec3(1.0f, 1.0f, 1.0f); //white light
+	lightColorLoc = glGetUniformLocation(myCustomShader.shaderProgram, "lightColor");
+	glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+
+	lightShader.useShaderProgram();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+
+time_t start;
+void renderScene()
+{
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	if(canControl)
+		processMovement();
+	
+
+
+	//render the scene to the depth buffer (first pass)
+
+	depthMapShader.useShaderProgram();
+
+	glUniformMatrix4fv(glGetUniformLocation(depthMapShader.shaderProgram, "lightSpaceTrMatrix"),
+		1,
+		GL_FALSE,
+		glm::value_ptr(computeLightSpaceTrMatrix()));
+
+	glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowMapFBO);
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	//create model matrix for nanosuit
+
+	
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+
+	
+	//send model matrix to shader
+	glUniformMatrix4fv(glGetUniformLocation(depthMapShader.shaderProgram, "model"),
+		1,
+		GL_FALSE,
+		glm::value_ptr(model));
+
+	myModel.Draw(depthMapShader);
+
+	//create model matrix for ground
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	//send model matrix to shader
+	glUniformMatrix4fv(glGetUniformLocation(depthMapShader.shaderProgram, "model"),
+		1,
+		GL_FALSE,
+		glm::value_ptr(model));
+
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	//render the scene (second pass)
+
+	myCustomShader.useShaderProgram();
+
+	
+
+	if (toggle_fog == 1)
+	{
+		glUniform1i(glGetUniformLocation(myCustomShader.shaderProgram, "setFog"), 1);
+	}
+	else
+	{
+		glUniform1i(glGetUniformLocation(myCustomShader.shaderProgram, "setFog"), 0);
+	}
+
+
+
+	//send lightSpace matrix to shader
+	glUniformMatrix4fv(glGetUniformLocation(myCustomShader.shaderProgram, "lightSpaceTrMatrix"),
+		1,
+		GL_FALSE,
+		glm::value_ptr(computeLightSpaceTrMatrix()));
+
+	//send view matrix to shader
+	view = myCamera.getViewMatrix();
+	glUniformMatrix4fv(glGetUniformLocation(myCustomShader.shaderProgram, "view"),
+		1,
+		GL_FALSE,
+		glm::value_ptr(view));
+
+	//compute light direction transformation matrix
+	lightDirMatrix = glm::mat3(glm::inverseTranspose(view));
+	//send lightDir matrix data to shader
+	glUniformMatrix3fv(lightDirMatrixLoc, 1, GL_FALSE, glm::value_ptr(lightDirMatrix));
+
+	glViewport(0, 0, retina_width, retina_height);
+	myCustomShader.useShaderProgram();
+
+	//bind the depth map
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+	glUniform1i(glGetUniformLocation(myCustomShader.shaderProgram, "shadowMap"), 3);
+
+	//create model matrix for nanosuit
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+	//send model matrix data to shader	
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	//compute normal matrix
+	normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+	//send normal matrix data to shader
+	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+	myModel.Draw(myCustomShader);
+
+	//satelite
+	//bind the depth map
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+	glUniform1i(glGetUniformLocation(myCustomShader.shaderProgram, "shadowMap"), 3);
+
+
+	sateliteAngle += 0.5f;
+	if (sateliteAngle > 360.0f)
+		sateliteAngle -= 360.0f;
+
+	//create model matrix for nanosuit
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(sateliteAngle), glm::vec3(0.15, 1, 0));
+	//send model matrix data to shader	
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	//compute normal matrix
+	normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+	//send normal matrix data to shader
+	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+	satelite.Draw(myCustomShader);
+
+	//create model matrix for ground
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	//send model matrix data to shader
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	//create normal matrix
+	normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+	//send normal matrix data to shader
+	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+
+	//draw a white cube around the light
+
+	lightShader.useShaderProgram();
+
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::translate(model, 1.0f * lightDir);
+	model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+	lightCube.Draw(lightShader);
+
+	lightShader.useShaderProgram();
+
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+	model = glm::rotate(glm::mat4(1.0f), -glm::radians(sateliteAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::translate(model, 2.0f * lightDir);
+	model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+	lightCube.Draw(lightShader);
+
+
+	lightShader.useShaderProgram();
+
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(sateliteAngle), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::translate(model, 2.0f * lightDir );
+	model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+	lightCube.Draw(lightShader);
+
+
+	double seconds_since_start = difftime(time(0), start);
+	if (seconds_since_start < 6) {
+		angle += 1.064f;
+		myCamera.move(gps::MOVE_FORWARD, 3 * cameraSpeed);
+	}
+	else if (seconds_since_start < 10) {
+		myCamera.move(gps::MOVE_FORWARD, 1.80f * cameraSpeed);
+	}
+	else {
+		canControl = true;
+	}
+
+	
+}
+
+int main(int argc, const char* argv[]) {
+
+	initOpenGLWindow();
+	initOpenGLState();
+	initFBOs();
+
+	std::vector<const GLchar*> faces;
+	faces.push_back("skybox/4/rt.tga");
+	faces.push_back("skybox/4/lf.tga");
+	faces.push_back("skybox/4/up.tga");
+	faces.push_back("skybox/4/dn.tga");
+	faces.push_back("skybox/4/bk.tga");
+	faces.push_back("skybox/4/ft.tga");
+	mySkyBox.Load(faces);
+	skyboxShader.loadShader("shaders/skyboxShader.vert", "shaders/skyboxShader.frag");
+	skyboxShader.useShaderProgram();
+
+	view = myCamera.getViewMatrix();
+	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+	projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	initModels();
+	initShaders();
+	initUniforms();
+	glCheckError();
+	glfwSetInputMode(glWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	start = time(0);
+	while (!glfwWindowShouldClose(glWindow)) {
+		renderScene();
+		mySkyBox.Draw(skyboxShader, view, projection);
+		glfwPollEvents();
+		glfwSwapBuffers(glWindow);
+	}
+
+	//close GL context and any other GLFW resources
+	glfwTerminate();
+
+	return 0;
+}
